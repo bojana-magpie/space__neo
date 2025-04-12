af79cf6c53626b0fa411d70dbd03c12ca96bf010
bojana-strachkovski <bojana.strachkovski@gmail.com>
2025-04-12 11:21:25 +0200

    add logo

---
+++ src/Components/Header/Logo/Logo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import LogoImage from "../../../assets/Img/spaceneo_logo.svg";
+
+const Logo = () => {
+    return (
+        <img src={LogoImage} alt="Logo" />
+    )
+}
+
+export default Logo
